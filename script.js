e807930a56b387cbf67251126356e4a96f9ae2b9
kang <lowkangxuan@gmail.com>
2025-06-09 00:26:09 +0800

    Fizz Buzz Assignment
    - Completed Fizz Buzz Assignment in script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+// function divisibleByNumber(toCheck, number) {
+//     return (toCheck % number === 0 );
+// }
+
+// for (let i = 1; i < answer; i++) {
+//     if (divisibleByNumber(i, 3) && divisibleByNumber(i, 5)) {
+//         console.log("Fizz Buzz");
+//         continue;
+//     }
+//     else if (divisibleByNumber(i, 3)) {
+//         console.log("Fizz");
+//         continue;
+//     }
+//     else if (divisibleByNumber(i, 5)) {
+//         console.log("Buzz");
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+console.log("Test".concat("Hi"));
