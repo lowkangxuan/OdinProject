9ff819018bb2a384b0321d9796f6dcddf7fd7877
kang <lowkangxuan@gmail.com>
2025-11-26 00:57:32 +0800

    Layout and style - Created svg folder for svg icons - modified global-style.css to facilitate img.icon and more colours - Still doing layout for main page

--- todo_list/src/index.js
+++ todo_list/src/index.js
@@ -2,3 +2,11 @@ import "./styles.css"
 import { greeting } from "./test";
 
 console.log(greeting);
+
+const listBtns = document.querySelectorAll(".list-btn");
+
+for (const btn of listBtns) {
+    btn.addEventListener("click", (e) => {
+        console.log("test");
+    });
+};
